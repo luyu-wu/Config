70a15afdb9a2b40ecf8549b0defbbe8ea9c1c565
luyu-wu <luyuwuvk@gmail.com>
2023-12-11 12:17:14 -0500

    special workspace support

--- hypr/Shaders/night.glsl
+++ hypr/Shaders/night.glsl
@@ -4,7 +4,7 @@ precision mediump float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
 
-const float temperature = 2600.0;
+const float temperature = 3000.0;
 const float temperatureStrength = 1.0;
 
 #define WithQuickAndDirtyLuminancePreservation
