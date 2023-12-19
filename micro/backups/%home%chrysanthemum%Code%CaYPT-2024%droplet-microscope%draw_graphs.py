720a5c9be35427b37474ecf6d7ea3303fd3d5dd2
luyu-wu <luyuwuvk@gmail.com>
2023-12-19 10:16:27 -0500

    changed a lot of stuff

---
+++ micro/backups/%home%chrysanthemum%Code%CaYPT-2024%droplet-microscope%draw_graphs.py
@@ -0,0 +1,14 @@
+import droplet_service as module
+import numpy as np
+import matplotlib as mtl
+
+
+def drawHeightVolume():
+    print("yes");
+
+def drawHeightSurfaceGas():
+    print("yes");
+
+def drawHeightSurfaceSolid():
+    print('yes');
+    
