d87b0e6ed62a7aece55106005df1046c8b6694ce
luyu-wu <luyuwuvk@gmail.com>
2024-01-13 13:29:49 -0500

    changed wallpaper and added notes workspaces

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,4 @@ end
 set -g fish_greeting
 starship init fish | source
 export "MICRO_TRUECOLOR=1"
-~/.config/fish/terminal_sunday.sh 2007-07-27 Luyu
+#~/.config/fish/terminal_sunday.sh 2007-07-27 Luyu
