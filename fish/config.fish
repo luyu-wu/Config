2ded5398451fc62d3820ade969b181b1d306851c
luyu-wu <luyuwuvk@gmail.com>
2024-03-09 14:01:01 -0500

    big light mode commit
    Former-commit-id: d435f04165edf852897e642395ee41970874c3fa
    Former-commit-id: 936536206a6d98f610bade3a2eee835ab672f8b1 [formerly 2d06ee6a535f560903cff628cb97a7f06c06e5ee]
    Former-commit-id: 30011f1290483cd9967817d11b8ba6f03559c8a9

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-set -g fish_greeting
-starship init fish | source
-export "MICRO_TRUECOLOR=1"
-#~/.config/fish/terminal_sunday.sh 2007-07-27 Luyu
