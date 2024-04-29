a03be56ad298978005373a128419fdb282b5cdd5
luyu-wu <luyuwuvk@gmail.com>
2024-04-29 15:43:25 -0400

    polish and apparently a lot of stuff 3min add time xd

---
+++ hypr/Scripts/switchdaemon/target/debug/deps/serde_repr-98909460cb0aff21.d
@@ -0,0 +1,6 @@
+/home/chrysanthemum/Code/Config/hypr/Scripts/switchdaemon/target/debug/deps/libserde_repr-98909460cb0aff21.so: /home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/lib.rs /home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/parse.rs
+
+/home/chrysanthemum/Code/Config/hypr/Scripts/switchdaemon/target/debug/deps/serde_repr-98909460cb0aff21.d: /home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/lib.rs /home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/parse.rs
+
+/home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/lib.rs:
+/home/chrysanthemum/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_repr-0.1.19/src/parse.rs:
